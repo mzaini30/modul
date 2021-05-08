33a3eaf1ecaf0379f418747b246ffca866e958ac
mzaini30 <muhzaini30@gmail.com>
2021-05-08 13:46:49 +0800

    upload

---
+++ stringify.js
@@ -0,0 +1,19 @@
+export default function queryStringify (obj, prefix) {
+  var pairs = []
+  for (var key in obj) {
+    if (!has.call(obj, key)) {
+      continue
+    }
+
+    var value = obj[key]
+    var enkey = encodeURIComponent(key)
+    var pair
+    if (typeof value === 'object') {
+      pair = queryStringify(value, prefix ? prefix + '[' + enkey + ']' : enkey)
+    } else {
+      pair = (prefix ? prefix + '[' + enkey + ']' : enkey) + '=' + encodeURIComponent(value)
+    }
+    pairs.push(pair)
+  }
+  return pairs.join('&')
+}
